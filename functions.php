5b331331f734d96fd7c9925dfb89f1a5c596bdc7
momotan039 <momotaha039@gmail.com>
2025-11-29 21:56:49 +0200

    feat: Add reusable helper functions for archives and contact page
    - Create helpers-archive.php with shared archive functions
    - Create helpers-contact.php for contact card rendering
    - Add centralized category filter HTML generation
    - Improve code reusability and maintainability

--- functions.php
+++ functions.php
@@ -14,14 +14,38 @@ define('ALOMRAN_THEME_DIR', get_template_directory());
 define('ALOMRAN_THEME_URI', get_template_directory_uri());
 
 $alomran_includes = array(
-    'inc/helpers.php',
+    // Core Setup
     'inc/setup.php',
     'inc/assets.php',
     'inc/menu-walker.php',
+    
+    // Helpers (modular)
+    'inc/helpers/helpers-company.php',
+    'inc/helpers/helpers-content.php',
+    'inc/helpers/helpers-products.php',
+    'inc/helpers/helpers-url.php',
+    'inc/helpers/helpers-archive.php',
+    'inc/helpers/helpers-contact.php',
+    
+    // Custom Post Types & Taxonomies
     'inc/cpt.php',
     'inc/taxonomies.php',
     'inc/acf.php',
+    
+    // Media Management
+    'inc/product-media.php',
+    'inc/news-media.php',
+    
+    // Contact & AJAX
+    'inc/contact-messages.php',
     'inc/ajax.php',
+    
+    // SEO
+    'inc/seo/seo-core.php',
+    'inc/seo/seo-schema.php',
+    'inc/seo/seo-sitemap.php',
+    
+    // Redux
     'inc/redux/redux-helpers.php',
     'inc/redux/redux-config.php',
 );
